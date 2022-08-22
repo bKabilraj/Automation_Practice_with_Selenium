00e5142ccc3f3bc3ace8b68a4988662a302eff53
bKabilraj <bkabilraj@gmail.com>
2022-08-22 20:37:09 +0530

    ToolTip

---
+++ Selenium Learning/src/selenium/ToolTip.java
@@ -0,0 +1,28 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ToolTip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", 
+				"C:\\Users\\HP\\Desktop\\Selenium_Learning\\chromedriver.exe\\");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/tooltip/");
+		
+		WebElement yourAge=driver.findElement(By.id("age"));
+		
+		String toolTipText = yourAge.getAttribute("title");
+		System.out.println("Actual Title of Tool Tip" +toolTipText); 
+	}
+
+}
+
+
+

ea1bf23b61c14f85866532ba2b9feaf078595d00
bKabilraj <bkabilraj@gmail.com>
2022-09-03 23:24:47 +0530

    Practice

--- Selenium Learning/src/selenium/FileDownloadExample2.java
+++ Selenium Learning/src/selenium/FileDownloadExample2.java
@@ -1,12 +1,13 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FileDownloadExample2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", 
@@ -14,6 +15,10 @@ public class FileDownloadExample2 {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.leafground.com/file.xhtml;jsessionid=node01j24lhhe9zde81lhctconh1uhd235686.node0");
 		
+		WebElement downloadButton = driver.findElement(By.linkText("Download"));
+		downloadButton.click();
+		
+		Thread.sleep(3000);
 		
 	}
 
